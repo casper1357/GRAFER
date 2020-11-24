69f9580dfee12017f27cea93872c2316691357e2
casper1357 <61235787+casper1357@users.noreply.github.com>
2020-11-24 21:01:40 +0100

    Add files via upload

---
+++ Differentialregning.py
@@ -0,0 +1,37 @@
+import sympy
+
+class Differentialregning:
+    def __init__(self, x):
+        self.x = x
+
+
+    # Metode der styrer hvilken formel vi skal bruge.
+
+    def func(self, x, forskrift):
+        y = sympy.sympify(forskrift).subs(dict(x=x))
+        return y
+
+    def tangent(self, x):
+        return self.a * x + self.b
+
+    def slopeforpoint(self, DeltaX, forskrift):
+        self.DeltaX = DeltaX
+        #self.inputpol = input("Enter differential polynomial here")
+        n = 0
+
+        # Vi bruger linjens ligning mellem 2 punkter for at finde hældningen på den linje der går igennem x og x + DeltaX
+        while True:
+            self.a = (self.func(self.x + self.DeltaX, forskrift) - self.func(self.x, forskrift)) / ((self.x + self.DeltaX) - self.x)
+
+            # Ved at dividere DeltaX med 2 hele tiden får vi den til at gå mod 0
+            # Dette betyder at vi kommer tættere på at have den tangent der går igennem x
+
+            #print(self.a)
+            self.DeltaX = self.DeltaX / 2
+
+            # Vi laver en variabel der hedder n så vi kører vores while loop 40 gange
+            n += 1
+            if n == 40:
+                self.b = self.func(self.x, forskrift) - self.a * self.x
+                #print("Skæringspunktet for differentialregningskurven er:", self.b)
+                return self.a
---
+++ Integralregning.py
@@ -0,0 +1,31 @@
+import sympy
+
+class Integralregning():
+    def __init__(self, a, b, columns):
+        self.a = a
+        self.b = b
+        self.columns = columns
+
+    def func(self, x, forskrift):
+        y = sympy.sympify(forskrift).subs(dict(x=x))
+        self.forskrift = forskrift
+        return y
+
+    def area(self, forskrift):
+        self.areaoffunc = 0
+        DeltaX = (self.b - self.a) / int(self.columns)
+        n = 0
+        x = self.a
+        while True:
+            x += DeltaX
+            y = self.func(x, forskrift)
+            columnareal = y * DeltaX
+            if columnareal > 0:
+                self.areaoffunc += float(columnareal)
+            else:
+                pass
+            n += 1
+            if n >= self.columns:
+                print("Arealet for integralkurven er:", self.areaoffunc)
+                return self.areaoffunc
+
